271bba1828cc78ecd0f344e8774a0d236212cd86
Getulio Sánchez <valentin2507@gmail.com>
2018-06-20 20:11:57 -0400

    Issue #2980854: Implement new Social Auth base test

--- tests/src/Functional/SocialAuthGithubSettingsFormTest.php
+++ tests/src/Functional/SocialAuthGithubSettingsFormTest.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\Tests\social_auth_github\Functional;
 
-use Drupal\social_api\SocialApiSettingsFormBaseTest;
+use Drupal\Tests\social_auth\Functional\SocialAuthTestBase;
 
 /**
  * Test Social Auth Github settings form.
@@ -11,7 +11,8 @@ use Drupal\social_api\SocialApiSettingsFormBaseTest;
  *
  * @ingroup social_auth_github
  */
-class SocialAuthGithubSettingsFormTest extends SocialApiSettingsFormBaseTest {
+class SocialAuthGithubSettingsFormTest extends SocialAuthTestBase {
+
   /**
    * Modules to enable.
    *
@@ -24,23 +25,33 @@ class SocialAuthGithubSettingsFormTest extends SocialApiSettingsFormBaseTest {
    */
   protected function setUp() {
     $this->module = 'social_auth_github';
-    $this->socialNetwork = 'github';
+    $this->provider = 'github';
     $this->moduleType = 'social-auth';
 
     parent::setUp();
   }
 
   /**
-   * {@inheritdoc}
+   * Test if implementer is shown in the integration list.
    */
   public function testIsAvailableInIntegrationList() {
     $this->fields = ['client_id', 'client_secret'];
 
-    parent::testIsAvailableInIntegrationList();
+    $this->checkIsAvailableInIntegrationList();
   }
 
   /**
-   * {@inheritdoc}
+   * Test if permissions are set correctly for settings page.
+   *
+   * @throws \Behat\Mink\Exception\ElementNotFoundException
+   * @throws \Behat\Mink\Exception\ExpectationException
+   */
+  public function testPermissionForSettingsPage() {
+    $this->checkPermissionForSettingsPage();
+  }
+
+  /**
+   * Test settings form submission.
    */
   public function testSettingsFormSubmission() {
     $this->edit = [
@@ -48,7 +59,7 @@ class SocialAuthGithubSettingsFormTest extends SocialApiSettingsFormBaseTest {
       'client_secret' => $this->randomString(10),
     ];
 
-    parent::testSettingsFormSubmission();
+    $this->checkSettingsFormSubmission();
   }
 
 }
