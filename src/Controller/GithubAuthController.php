144e488c3c960c1557209b143cc9a6b9138fd9c2
Getulio Sánchez <valentin2507@gmail.com>
2018-07-28 13:06:18 -0400

    Issue #2988839: Use social_auth.user_authenticator service

--- src/Controller/GithubAuthController.php
+++ src/Controller/GithubAuthController.php
@@ -4,17 +4,17 @@ namespace Drupal\social_auth_github\Controller;
 
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\social_api\Plugin\NetworkManager;
-use Drupal\social_auth\Controller\SocialAuthOAuth2ControllerBase;
+use Drupal\social_auth\Controller\OAuth2ControllerBase;
 use Drupal\social_auth\SocialAuthDataHandler;
-use Drupal\social_auth\SocialAuthUserManager;
+use Drupal\social_auth\User\UserAuthenticator;
 use Drupal\social_auth_github\GithubAuthManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
- * Returns responses for Simple Github Connect module routes.
+ * Returns responses for Social Auth Github routes.
  */
-class GithubAuthController extends SocialAuthOAuth2ControllerBase {
+class GithubAuthController extends OAuth2ControllerBase {
 
   /**
    * GithubAuthController constructor.
@@ -23,7 +23,7 @@ class GithubAuthController extends SocialAuthOAuth2ControllerBase {
    *   The messenger service.
    * @param \Drupal\social_api\Plugin\NetworkManager $network_manager
    *   Used to get an instance of social_auth_google network plugin.
-   * @param \Drupal\social_auth\SocialAuthUserManager $user_manager
+   * @param \Drupal\social_auth\User\UserAuthenticator $user_authenticator
    *   Manages user login/registration.
    * @param \Drupal\social_auth_github\GithubAuthManager $github_manager
    *   Used to manage authentication methods.
@@ -34,12 +34,12 @@ class GithubAuthController extends SocialAuthOAuth2ControllerBase {
    */
   public function __construct(MessengerInterface $messenger,
                               NetworkManager $network_manager,
-                              SocialAuthUserManager $user_manager,
+                              UserAuthenticator $user_authenticator,
                               GithubAuthManager $github_manager,
                               RequestStack $request,
                               SocialAuthDataHandler $data_handler) {
 
-    parent::__construct('Social Auth Github', 'social_auth_github', $messenger, $network_manager, $user_manager, $github_manager, $request, $data_handler);
+    parent::__construct('Social Auth Github', 'social_auth_github', $messenger, $network_manager, $user_authenticator, $github_manager, $request, $data_handler);
   }
 
   /**
@@ -49,7 +49,7 @@ class GithubAuthController extends SocialAuthOAuth2ControllerBase {
     return new static(
       $container->get('messenger'),
       $container->get('plugin.network.manager'),
-      $container->get('social_auth.user_manager'),
+      $container->get('social_auth.user_authenticator'),
       $container->get('social_auth_github.manager'),
       $container->get('request_stack'),
       $container->get('social_auth.data_handler')
@@ -63,17 +63,17 @@ class GithubAuthController extends SocialAuthOAuth2ControllerBase {
    */
   public function callback() {
 
-    /* @var \League\OAuth2\Client\Provider\GithubResourceOwner $profile */
+    /* @var \League\OAuth2\Client\Provider\GithubResourceOwner|null $profile */
     $profile = $this->processCallback();
 
     // If authentication was successful.
     if ($profile !== NULL) {
 
       // Gets (or not) extra initial data.
-      $data = $this->userManager->checkIfUserExists($profile->getId()) ? NULL : $this->providerManager->getExtraDetails();
+      $data = $this->userAuthenticator->checkProviderIsAssociated($profile->getId()) ? NULL : $this->providerManager->getExtraDetails();
 
       // If user information could be retrieved.
-      return $this->userManager->authenticateUser($profile->getName(), $profile->getEmail(), $profile->getId(), $this->providerManager->getAccessToken(), $profile->toArray()['avatar_url'], $data);
+      return $this->userAuthenticator->authenticateUser($profile->getName(), $profile->getEmail(), $profile->getId(), $this->providerManager->getAccessToken(), $profile->toArray()['avatar_url'], $data);
     }
 
     return $this->redirect('user.login');
